f0819da19d2dfabf69bb4b72af5b9e8af82ddbd6
Ben Clayton <bclayton@google.com>
2024-02-12 15:09:19 +0000

    WIP: textureDimensions

--- src/webgpu/shader/execution/expression/call/builtin/textureDimension.spec.ts
+++ src/webgpu/shader/execution/expression/call/builtin/textureDimension.spec.ts
@@ -8,6 +8,7 @@ If level is outside the range [0, textureNumLevels(t)) then any valid value for 
 `;
 
 import { makeTestGroup } from '../../../../../../common/framework/test_group.js';
+import { kAllTextureFormats, kTextureFormatInfo } from '../../../../../format_info.js';
 import { GPUTest } from '../../../../../gpu_test.js';
 
 export const g = makeTestGroup(GPUTest);
@@ -38,23 +39,57 @@ Parameters:
    - The mip level, with level 0 containing a full size version of the texture.
    - If omitted, the dimensions of level 0 are returned.
 `
-  )
-  .params(u =>
+  ).params(u =>
     u
-      .combine('texture_type', [
-        'texture_1d',
-        'texture_2d',
-        'texture_2d_array',
-        'texture_3d',
-        'texture_cube',
-        'texture_cube_array',
-        'texture_multisampled_2d',
-      ] as const)
-      .beginSubcases()
-      .combine('sampled_type', ['f32-only', 'i32', 'u32'] as const)
-      .combine('level', [undefined, 0, 1, 'textureNumLevels', 'textureNumLevels+1'] as const)
+      .combine('format', kAllTextureFormats)
+      .combine('dimensions', [[8, 8], [16, 8], [8, 32]] as const)
   )
-  .unimplemented();
+  .fn(t => {
+    const formatInfo = kTextureFormatInfo[t.params.format];
+    const texture = t.device.createTexture({
+      size: t.params.dimensions,
+      usage: GPUTextureUsage.RENDER_ATTACHMENT | GPUTextureUsage.COPY_SRC,
+      format: t.params.format,
+    });
+    // TODO: Add tests for texture views
+    const textureView = texture.createView();
+    const wgsl = `
+@group(0) @binding(0) var texture : texture_2d<${formatInfo.sampleType}>;
+@group(0) @binding(1) var output : vec${t.params.dimensions.length}u;
+
+@compute @workgroup_size(1)
+fn main() {
+  output = textureDimensions(texture);
+}
+`;
+    const module = t.device.createShaderModule({
+      code: wgsl,
+    });
+    const pipeline = t.device.createComputePipeline({
+      compute: { module },
+      layout: 'auto',
+    });
+    const outputBuffer = t.device.createBuffer({
+      size: 32,
+      usage: GPUBufferUsage.COPY_SRC | GPUBufferUsage.STORAGE,
+    });
+    const bindgroup = t.device.createBindGroup({
+      layout: pipeline.getBindGroupLayout(0),
+      entries: [
+        { binding: 0, resource: { buffer: outputBuffer } },
+        { binding: 1, resource: textureView },
+      ],
+    });
+    const encoder = t.device.createCommandEncoder();
+    const pass = encoder.beginComputePass();
+    pass.setPipeline(pipeline);
+    pass.setBindGroup(0, bindgroup);
+    pass.dispatchWorkgroups(1);
+    pass.end();
+    t.device.queue.submit([encoder.finish()]);
+
+    t.expectGPUBufferValuesEqual(outputBuffer, new Uint32Array(t.params.dimensions));
+  });
 
 g.test('depth')
   .specURL('https://www.w3.org/TR/WGSL/#texturedimensions')
